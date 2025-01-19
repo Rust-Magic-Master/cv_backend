869ea30c37e97808cce8d005d00d22f2be59548f
eeee0717 <chentao020717Work@outlook.com>
2025-01-19 15:58:15 +0800

    feat: pass img to gray and save to local dir

--- src/routes/test/post.rs
+++ src/routes/test/post.rs
@@ -1,23 +1,40 @@
 use axum::http::StatusCode;
 use axum::Json;
-use opencv::imgcodecs;
+use opencv::imgproc::COLOR_BGR2GRAY;
 use opencv::prelude::*;
-use serde::{Deserialize, Serialize};
+use opencv::{imgcodecs, imgproc::cvt_color_def};
+use serde::Deserialize;
 #[derive(Debug, Deserialize, Clone)]
 pub struct PostRequest {
-    pub file_path: String,
+    pub root_path: String,
+    pub file_name: String,
 }
-#[derive(Debug, Serialize)]
-pub struct PostResponse {
-    pub image: String,
+pub fn get_image(body: Json<PostRequest>) -> Result<Mat, StatusCode> {
+    let root_path = &body.0.root_path;
+    let file_name = &body.0.file_name;
+    let image_path = format!("{}/{}", root_path, file_name);
+    println!("file path:{:?}", root_path);
+    println!("file name:{:?}", file_name);
+    let img = match imgcodecs::imread(&image_path, imgcodecs::IMREAD_COLOR) {
+        Ok(img) => img,
+        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+    };
+    Ok(img)
+}
+pub fn pass_to_gray(img: Mat) -> Result<Mat, StatusCode> {
+    let mut img_gray = Mat::default();
+    match cvt_color_def(&img, &mut img_gray, COLOR_BGR2GRAY) {
+        Ok(it) => it,
+        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+    };
+    Ok(img_gray)
 }
 #[axum::debug_handler]
 pub async fn post_test(body: Json<PostRequest>) -> Result<(), StatusCode> {
-    // 加载图像
-    let image_path = &body.0.file_path;
-    println!("image path:{:?}", image_path);
-    let img = imgcodecs::imread(image_path, imgcodecs::IMREAD_COLOR).unwrap();
-    // 打印图像信息
+    let img = get_image(body.clone()).unwrap();
     println!("Image dimensions: {}x{}", img.cols(), img.rows());
+    let img_gray = pass_to_gray(img).unwrap();
+    let img_gray_path = format!("{}/gray.jpg", body.0.root_path);
+    imgcodecs::imwrite(&img_gray_path, &img_gray, &opencv::core::Vector::new()).unwrap();
     Ok(())
 }
