eb893652118bdb60a06c4b30ea9962fbd2e55998
eeee0717 <chentao020717Work@outlook.com>
2025-01-19 16:53:42 +0800

    feat: finish show processed image

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::{routing::post, Router};
 use routes::test::post::*;
+use tower_http::services::ServeDir;
 mod routes;
 #[tokio::main]
 async fn main() {
     println!("Server Start");
     tracing_subscriber::fmt::init();
-    let app = Router::new().route("/test", post(post_test));
+    let app = Router::new()
+        .nest_service("/assets", ServeDir::new("assets"))
+        .route("/test", post(post_test));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
--- src/routes/test/post.rs
+++ src/routes/test/post.rs
@@ -3,19 +3,23 @@ use axum::Json;
 use opencv::imgproc::COLOR_BGR2GRAY;
 use opencv::prelude::*;
 use opencv::{imgcodecs, imgproc::cvt_color_def};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::env;
 #[derive(Debug, Deserialize, Clone)]
 pub struct PostRequest {
-    pub root_path: String,
     pub file_name: String,
 }
+#[derive(Debug, Serialize)]
+pub struct PostResoponse {
+    pub processed_file_name: String,
+}
+
 pub fn get_image(body: Json<PostRequest>) -> Result<Mat, StatusCode> {
-    let root_path = &body.0.root_path;
     let file_name = &body.0.file_name;
-    let image_path = format!("{}/{}", root_path, file_name);
-    println!("file path:{:?}", root_path);
-    println!("file name:{:?}", file_name);
-    let img = match imgcodecs::imread(&image_path, imgcodecs::IMREAD_COLOR) {
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let photos_folder = current_dir.join("assets");
+    let image_path = photos_folder.join(file_name);
+    let img = match imgcodecs::imread(image_path.to_str().unwrap(), imgcodecs::IMREAD_COLOR) {
         Ok(img) => img,
         Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
     };
@@ -30,11 +34,12 @@ pub fn pass_to_gray(img: Mat) -> Result<Mat, StatusCode> {
     Ok(img_gray)
 }
 #[axum::debug_handler]
-pub async fn post_test(body: Json<PostRequest>) -> Result<(), StatusCode> {
+pub async fn post_test(body: Json<PostRequest>) -> Result<Json<PostResoponse>, StatusCode> {
     let img = get_image(body.clone()).unwrap();
     println!("Image dimensions: {}x{}", img.cols(), img.rows());
     let img_gray = pass_to_gray(img).unwrap();
-    let img_gray_path = format!("{}/gray.jpg", body.0.root_path);
-    imgcodecs::imwrite(&img_gray_path, &img_gray, &opencv::core::Vector::new()).unwrap();
-    Ok(())
+    imgcodecs::imwrite("assets/gray.jpg", &img_gray, &opencv::core::Vector::new()).unwrap();
+    Ok(Json(PostResoponse {
+        processed_file_name: String::from("gray.jpg"),
+    }))
 }
